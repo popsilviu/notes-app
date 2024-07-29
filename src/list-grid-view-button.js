a6676f46fd0d22646b63b73eb2c5fc94306c9c78
Cozma Rares <cozmarares11@gmail.com>
2024-07-29 17:39:47 +0300

    feature/list-grid-view-button: store layout state in local storage

--- src/list-grid-view-button.js
+++ src/list-grid-view-button.js
@@ -1,18 +1,28 @@
-const listGridButton = document.querySelector(
-  ".list-grid-button[data-view]",
-);
+function setLayout(layout) {
+  const notesContainer = document.querySelector("[data-notes-container]");
+
+  if (layout === "grid") {
+    notesContainer.classList.add("notes-grid");
+    listGridButton.dataset.view = "grid";
+  } else if (layout === "list") {
+    notesContainer.classList.remove("notes-grid");
+    listGridButton.dataset.view = "list";
+  } else throw new Error(`Unknown layout: ${layout}`);
+
+  localStorage.setItem("layout", layout);
+}
+
+const listGridButton = document.querySelector(".list-grid-button[data-view]");
 
 listGridButton.onclick = () => {
   const notesContainer = document.querySelector("[data-notes-container]");
-  const listGridButton = document.querySelector(
-    ".list-grid-button[data-view]",
-  );
+  const listGridButton = document.querySelector(".list-grid-button[data-view]");
 
-  if (notesContainer.classList.contains("notes-grid")) {
-    notesContainer.classList.remove("notes-grid");
-    listGridButton.dataset.view = "list";
-  } else {
-    notesContainer.classList.add("notes-grid");
-    listGridButton.dataset.view = "grid";
-  }
+  if (notesContainer.classList.contains("notes-grid")) setLayout("list");
+  else setLayout("grid");
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  const layout = localStorage.getItem("layout") ?? "grid";
+  setLayout(layout);
+});
