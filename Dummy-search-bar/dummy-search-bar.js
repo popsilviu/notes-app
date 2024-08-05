260328c9e5889b78b198e8dd02a06820d915d757
Vlad Pop <vlad.pop0797@gmail.com>
2024-08-05 15:39:09 +0300

    Used BEM naming, replaced focus, focusout js events with focus-within pseudoclass

--- Dummy-search-bar/dummy-search-bar.js
+++ Dummy-search-bar/dummy-search-bar.js
@@ -1,21 +1,9 @@
-let search_bar = document.getElementById("top-search-bar");
-let search_bar_input = document.querySelector(".search-bar__input");
-let search_bar_button = document.querySelector(".search-bar__button");
+let search_bar_input = document.querySelector(".form__input");
+let search_bar_button = document.querySelector(".form__button--search");
 
-
-search_bar_input.addEventListener("focusout", function() {search_bar.classList.remove("search-bar--focused")});
-search_bar_input.addEventListener("focus", function() {search_bar.classList.add("search-bar--focused")});
 search_bar_button.addEventListener("click", searchButtonClick);
 search_bar_input.addEventListener("keydown", function(event) {if(event.key === "Enter") {event.preventDefault();}});
 search_bar_input.addEventListener("keydown", function(event) {if((event.key === "Enter") && (search_bar_input.value)) {initiateSearch();}});
-
-function searchBarClick() {
-    console.log("clicked search bar");
-    if(!search_bar.classList.contains("search-bar--focused")) {
-        search_bar.classList.add("search-bar--focused");
-        search_bar_input.focus();
-    }
-}
 
 function searchButtonClick() {
     if(search_bar_input.value) {
