a965fe6dd6c12b718bcb186f2b1eed2cfcbaeaaa
Cozma Rares <cozmarares11@gmail.com>
2024-08-13 11:21:24 +0300

    fix/grid-list-view-technical-debt: remove reduntant onclick event listener & adopt BEM naming for CSS classes

--- src/list-grid-view-button.js
+++ src/list-grid-view-button.js
@@ -1,26 +1,28 @@
+const layoutModifierClassName = "notes__container--grid__layout";
+
 function setLayout(layout) {
-  const notesContainer = document.querySelector("[data-notes-container]");
+  const notesContainer = document.querySelector("#notesContainer");
+  const layoutToggleButton = document.querySelector("#layoutToggleButton");
 
   if (layout === "grid") {
-    notesContainer.classList.add("notes-grid");
-    listGridButton.dataset.view = "grid";
+    notesContainer.classList.add(layoutModifierClassName);
+    layoutToggleButton.dataset.view = "grid";
   } else if (layout === "list") {
-    notesContainer.classList.remove("notes-grid");
-    listGridButton.dataset.view = "list";
+    notesContainer.classList.remove(layoutModifierClassName);
+    layoutToggleButton.dataset.view = "list";
   } else throw new Error(`Unknown layout: ${layout}`);
 
   localStorage.setItem("layout", layout);
 }
 
-const listGridButton = document.querySelector(".list-grid-button[data-view]");
+function toggleLayout() {
+  const notesContainer = document.querySelector("#notesContainer");
+  const listGridButton = document.querySelector("#layoutToggleButton");
 
-listGridButton.onclick = () => {
-  const notesContainer = document.querySelector("[data-notes-container]");
-  const listGridButton = document.querySelector(".list-grid-button[data-view]");
-
-  if (notesContainer.classList.contains("notes-grid")) setLayout("list");
+  if (notesContainer.classList.contains(layoutModifierClassName))
+    setLayout("list");
   else setLayout("grid");
-};
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const layout = localStorage.getItem("layout") ?? "grid";
