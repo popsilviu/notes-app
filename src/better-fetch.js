62e4487584bf91a89e25cddb54d9b7068ecaa0ce
Cozma Rares <cozmarares11@gmail.com>
2024-08-27 15:50:56 +0300

    feature/better-fetch: add timeout to fetch2
    Add support for request timeouts in `fetch2`, if a `timeoutMs` option is
    provided and no other `signal` is present in the `options` object.

--- src/better-fetch.js
+++ src/better-fetch.js
@@ -1,14 +1,22 @@
 class FetchError extends Error {
   constructor(res) {
-    super("Bad fetch response");
+    super(`Bad fetch response: ${res.status}`);
     this.response = res;
   }
 }
 
-async function fetch2(...options) {
-  const res = await fetch(...options);
-  if (!res.ok) {
-    throw new FetchError(res);
-  }
+async function fetch2(url, { timeoutMs = -1, ...options } = {}) {
+  if (
+    typeof options == "object" &&
+    !("signal" in options) &&
+    typeof timeoutMs == "number" &&
+    timeoutMs > 0
+  )
+    options.signal = AbortSignal.timeout(timeoutMs);
+
+  const res = await fetch(url, options);
+  if (!res.ok) throw new FetchError(res);
   return res;
 }
+
+fetch2("https://httpbin.org/delay/10", { timeoutMs: 3000 });
